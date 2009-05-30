06e612d99fda8759e79278d8be48d8536ad0b6d5
Michael Leung <blamemike@gmail.com>
2009-05-30 15:04:42 -0400

    Got rid of the raise_if_no_block method. Not needed.

--- lib/ie_no_more.rb
+++ lib/ie_no_more.rb
@@ -7,22 +7,15 @@ module IENoMore
   end
   
   def hide_if_ie6
-    raise_if_no_block
     yield unless is_ie6?
   end
   
   def show_if_ie6
-    raise_if_no_block
     yield if is_ie6?
   end
   
   def is_ie6?
     request.env["HTTP_USER_AGENT"] =~ /MSIE 6+?/
   end
-  
-  private
-    def raise_if_no_block
-      raise 'We need a block here' unless block_given?
-    end
     
 end
